8a963e15282d267301a126d910bedb092b886696
Maggus <mail4markus.schneider@gmx.de>
2023-03-07 13:34:01 +0100

    feat: add a board for notifications

--- src/components/notifications-board/notifications-board.tsx
+++ src/components/notifications-board/notifications-board.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useFetchNotifications } from '../../hooks/use-fetch-notifications';
 import { Badge } from '../badge';
+import { NotificationsBoardItem } from './notifications-board-item';
 import './notifications-board.css';
 
 export function NotificationsBoard() {
+  const notifications = useFetchNotifications();
+
+  const getUnreadNotifications = () => notifications.filter((x) => !x.read).length;
+
   return (
     <div className="notifications-board-container">
       <div className="header">
         <div className="left-side">
           <h2>Notifications</h2>
-          <Badge backgroundcolor="blue" count={3} />
+          <Badge backgroundcolor="blue" count={getUnreadNotifications()} />
         </div>
         <div className="right-side">
           <button>Mark all as read</button>
         </div>
+      </div>
+      <div className="notification-list">
+        {notifications.map((notification, index) => (
+          <NotificationsBoardItem key={`notification-item-${notification.image}`} notification={notification} />
+        ))}
       </div>
     </div>
   );
